f152729bc22b5311ccd679a4e45c1c2e1d2657dc
shun3007 <s3007@outlook.jp>
2019-11-16 23:24:06 +0900

    first commit

---
+++ git_c.php
@@ -0,0 +1,15 @@
+<?php
+$info = 'これはgit_c.phpです';
+echo 'これはgit_c.phpです';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>課題20 Git その3</title>
+<meta charset="utf-8">
+</head>
+<body>
+<p><?php echo $info;?></p>
+</body>
+</html>
+
