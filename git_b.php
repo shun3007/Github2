99ff103bd9f26f24b288dbf4957f4cc557ff7f58
shun3007 <s3007@outlook.jp>
2019-11-16 23:10:45 +0900

    Git start

---
+++ git_b.php
@@ -0,0 +1,14 @@
+<?php
+$info = 'これはgit_b.phpです';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>課題20 Git その2</title>
+<meta charset="utf-8">
+</head>
+<body>
+<p><?php echo $info;?></p>
+</body>
+</html>
+
